eda300087f549afe07d4020ddfd88b35207d3522
RicardoSilvaOititec <ricardo.silva@oititec.com.br>
2025-01-28 09:00:41 -0300

    fisrst commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,41 @@
-import type { NextConfig } from "next";
+import CopyPlugin from 'copy-webpack-plugin';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: { unoptimized: true },
+  output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack: (config) => {
+    // append the CopyPlugin to copy the file to your public dir
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: './node_modules/@oiti/facetec-sdk/core-sdk/FaceTec_images',
+            to: './static/chunks/facetec/FaceTec_images',
+          },
+          {
+            from: './node_modules/@oiti/facetec-sdk/core-sdk/FaceTecSDK.js/resources',
+            to: './static/chunks/facetec/resources',
+          },
+          {
+            from: './node_modules/@oiti/facetec-sdk/app-resources',
+            to: './static/chunks/facetec/app-resources',
+          },
+          {
+            from: './node_modules/@oiti/sdk-web/dist/src/shared/public',
+            to: 'public/',
+          },
+        ],
+      })
+    );
+
+    // Important: return the modified config
+    return config;
+  },
 };
 
 export default nextConfig;
